27fce9b45a1dab82316c7391b65e5a09e3ce4df7
Alok Nayan <aloknayan@Aloks-MacBook-Air.local>
2024-03-01 12:35:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const options = ["cricket", "football", "hockey"];
+  const days = ["weekdays", "weekend"];
+  const [selectedOption, setSelectedOption] = useState("");
+  const [selectedDay, setSelectedDay] = useState("");
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  const handleDayChange = (event) => {
+    setSelectedDay(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Select an option:</h2>
+      {options.map((option, index) => (
+        <label key={index}>
+          <input
+            type="radio"
+            name="option"
+            value={option}
+            checked={selectedOption === option}
+            onChange={handleOptionChange}
+          />
+          {option}
+        </label>
+      ))}
+      <h2>Select a day:</h2>
+      {days.map((day, index) => (
+        <label key={index}>
+          <input
+            type="radio"
+            name="day"
+            value={day}
+            checked={selectedDay === day}
+            onChange={handleDayChange}
+          />
+          {day}
+        </label>
+      ))}
+      <div>Selected option: {selectedOption}</div>
+      <div>Selected day: {selectedDay}</div>
     </div>
   );
 }
